c88c523fa6fa3a26e5b857d5dda3d0ee4681a152
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2023-05-03 19:38:49 -0300

    meu jeitinho
    essa e minha conlusao do projeto

--- app.js
+++ app.js
@@ -18,3 +18,106 @@ const getTypeColor = type => {
     fighting: '#E6E0D4'
   }[type] || normal
 }
+
+// contagens de scroll
+let itemsPorPagina = 15;
+let numeroDeItensCarregados = 0;
+let cont = 0
+
+const fragment = document.createDocumentFragment()
+const pokemon_list = document.querySelector(".pokedex");
+
+function criarTags(poke_array) {
+  const card = document.createElement("li")
+  card.classList.add("card");
+  card.style.setProperty('--type-color',getTypeColor(poke_array[2][0]))
+  
+  fragment.appendChild(card);
+
+  // tags dentro do card 
+  const conteudo_card = [
+    {type: "img",classe:"card-image"},
+    {type: "h1",classe:"card-title"},
+    {type: "h2",classe:"card-subtitle"}]
+  
+    
+  conteudo_card.forEach((child,indice) => {
+    const element = document.createElement(child.type);
+    element.classList.add(child.classe)
+    
+    
+    if(child.type == "img"){
+      element.src = "./assets/img/"+poke_array[1]+".png"
+    }
+    else{
+      if (child.type == "h1") {
+        element.innerText =poke_array[indice] +". "+ poke_array[indice -1] 
+      }else{
+        element.innerText = poke_array[indice]
+      }
+      
+    }
+   
+    
+  
+    card.appendChild(element);
+  });  
+
+}
+
+async function  apiPokemon() {
+  const dados_poke = []
+  for(let i = 1;i<=151;i++){
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(T => T.json())
+    dados_poke.push(
+      {
+        nome:pokemon.forms[0].name,
+        id:pokemon.id,
+        tipo:pokemon.types && pokemon.types.map(element=>element.type.name)
+      }
+    )
+
+   
+  }
+   return dados_poke
+}
+
+
+
+async function main() {
+  
+  const dados = await apiPokemon();
+  const itensIniciais = dados.slice(numeroDeItensCarregados, itemsPorPagina);
+  itensIniciais.forEach(element=>{
+    let var_array = []
+    for(e in element){
+      var_array.push(element[e]);
+    }
+    criarTags(var_array);
+    
+  }) 
+  pokemon_list.append(fragment)
+  console.log(numeroDeItensCarregados,itemsPorPagina);
+  numeroDeItensCarregados +=15
+  itemsPorPagina += 15
+
+  
+  
+}
+main()
+
+
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight ) {
+    
+    main()
+    
+  }
+});
+
+
+
+
